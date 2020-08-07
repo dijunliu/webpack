ed1f42ea7c0ddf6ca830de531900018462dd3a79
dijunliu <dijunliu@dijunliudeiMac.local>
2020-08-07 21:14:06 +0800

    error

--- src/number.js
+++ src/number.js
@@ -1,6 +1,6 @@
 export default function number() {
     const div = document.createElement("button")
     div.setAttribute("id", "number")
-    div.innerHTML = 999
+    div.innerHTML = 11999
     document.body.append(div)
 }
